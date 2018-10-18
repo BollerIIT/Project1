fe0a49b6ff8425598ebedc8847b22bfd01e2ddfd
Tboller <tboller@hawk.iit.edu>
2018-10-17 20:23:56 -0500

    Added some small amount of javascript to gather the data and alert the user

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+
+
+
+	var subButton = document.getElementById('submit');
+	
+	function submitBtnHandler () {
+		//tests the button was clicked
+		console.log('submit clicked');
+		
+		//stores the form data for use
+		var name = document.getElementById('name');
+		var email = document.getElementById('email');
+		var phone = document.getElementById('pNumber');
+		var birthDat = document.getElementById('dob');
+		
+		//Alerts the user that their data was submited
+		alert("Your information was received, someone will contact you shortly");
+		
+		
+		
+
+	
+	document.getElementById('submit).addEventListener('click', runBtnHandler, false);
+	
