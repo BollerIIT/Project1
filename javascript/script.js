1a336f915418e139524f4c9aa79cdb524cd9c3a9
Tboller <tboller@hawk.iit.edu>
2018-10-28 06:18:33 -0500

    Tyring to get javascript to work, changed sizedtextboxes to class

--- javascript/script.js
+++ javascript/script.js
@@ -10,14 +10,29 @@ Basic form data collection
 		//tests the button was clicked
 		console.log('submit clicked');
 		
+		var pattern =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 		//stores the form data for use
 		var name = document.getElementById('name');
 		var email = document.getElementById('email');
+		console.log(email);
 		var phone = document.getElementById('pNumber');
 		var birthDat = document.getElementById('dob');
+		alert(email);
 		
-		//Alerts the user that their data was submited
-		alert("Your information was received, someone will contact you shortly");
+var regexEmail = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+		//var email = document.getElementById('email');
+
+		if (regexEmail.test(email.value)) {
+			alert("It's Okay")
+		} else {
+			alert("Not Okay")
+
+		}
+
 	};
 		
 		
+	function submitForm () {		
+		//Alerts the user that their data was submited
+		alert("Your information was received, someone will contact you shortly");		
+	}
